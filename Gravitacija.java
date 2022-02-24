cd01036687e5fc1df47a9b5781c0d8b08981a6e8
aanja123 <aa1737@student.uni-lj.si>
2022-02-24 19:29:07 +0100

    gravitacija

---
+++ Gravitacija.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Gravitacija {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("OIS je zakon!");
+	}
+}
