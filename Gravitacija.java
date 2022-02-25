0fe507ab98da6de1204733d29bfc3c02fbcefed0
Domen Lemut <domen.lemut@gmail.com>
2022-02-25 09:27:31 +0100

    Add files via upload

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,9 +1,26 @@
 import java.util.*;
+import java.lang.Math;
 
-public class Gravitacija {
-
+public class Gravitacija2 {
     public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("OIS je zakon!");
-	}
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("OIS je zakon!");
+    	double v = sc.nextDouble();
+    	System.out.println(izracun(v));
+    }
+    public static double izracun(double v){
+        double C = 6.674*Math.pow(10, -11);
+        double M = 5.972*Math.pow(10, 24);
+        double r = 6.371*Math.pow(10, 6);
+        double a = (C*M)/((r+v)*(r+v));
+		return a;
+    }
+    public static void izpis(double v){
+       double C = 6.674*Math.pow(10, -11);
+       double M = 5.972*Math.pow(10, 24);
+       double r = 6.371*Math.pow(10, 6);
+       double a = (C*M)/((r+v)*(r+v));
+       System.out.println(v);
+       System.out.println(a);
+    }
 }
