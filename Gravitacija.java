502f781fdd581e3192a04a3091af641ff33115be
aanja123 <aa1737@student.uni-lj.si>
2022-02-25 20:29:40 +0100

    Dodana slika

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 
-public class Gravitacija2 {
+public class Gravitacija {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	System.out.println("OIS je zakon!");
